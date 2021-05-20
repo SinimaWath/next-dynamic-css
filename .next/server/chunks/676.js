1177acaa0a1a471f07fafd28baf6e5e0dfdbc268
Vladislav Tarasov <vlad-tarasov@yandex-team.ru>
2021-05-20 16:25:32 +0300

    fix babel

---
+++ .next/server/chunks/676.js
@@ -0,0 +1,66 @@
+exports.id = 676;
+exports.ids = [676];
+exports.modules = {
+
+/***/ 676:
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": function() { return /* binding */ Content; }
+});
+
+// EXTERNAL MODULE: external "react"
+var external_react_ = __webpack_require__(297);
+var external_react_default = /*#__PURE__*/__webpack_require__.n(external_react_);
+// EXTERNAL MODULE: ./src/Component2.module.scss
+var Component2_module = __webpack_require__(361);
+var Component2_module_default = /*#__PURE__*/__webpack_require__.n(Component2_module);
+;// CONCATENATED MODULE: ./src/Component2.jsx
+
+var __jsx = (external_react_default()).createElement;
+
+function Content2() {
+  return __jsx("div", {
+    className: (Component2_module_default()).container
+  }, __jsx("h1", {
+    className: (Component2_module_default()).header
+  }, "Where does it come from?"), __jsx("div", {
+    className: (Component2_module_default()).textContent
+  }, "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham."));
+}
+;// CONCATENATED MODULE: ./src/Content.jsx
+
+var Content_jsx = (external_react_default()).createElement;
+
+function Content() {
+  return Content_jsx("div", {
+    className: styles.container
+  }, Content_jsx("h1", {
+    className: styles.header
+  }, "Where does it come from?"), Content_jsx("div", {
+    className: styles.textContent
+  }, "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham."), Content_jsx(Content2, null));
+}
+
+/***/ }),
+
+/***/ 361:
+/***/ (function(module) {
+
+// Exports
+module.exports = {
+	"header": "Component2_header__3kwHY",
+	"container": "Component2_container__3Yehs",
+	"textContent": "Component2_textContent__3iBGn"
+};
+
+
+/***/ })
+
+};
+;
---
+++ .next/static/WfnPmHsq5hQXgVo3Sugds/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-826738477417a09c500d.js"],"/_error":["static/chunks/pages/_error-dccbde1790fb6f808916.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
